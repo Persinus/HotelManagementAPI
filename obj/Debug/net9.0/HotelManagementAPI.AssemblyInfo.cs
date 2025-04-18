a6ef997136996af229f508ba57a45ee4166a0855
khimphm <phamtrikhiem2524@gmail.com>
2025-04-19 00:05:51 +0700

    them_note.txt

--- obj/Debug/net9.0/HotelManagementAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/HotelManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c0f96d3d42c85494f7e2a29f862e65f18ffd0b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e621d36c2a8fd3e4533b77dd41f8f3a5320db05d")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
