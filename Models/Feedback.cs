d67ea07b8de44589404b33e93c7fb19931c6c137
Persinus <nguyenmanh2004devgame@gmail.com>
2025-05-10 02:39:02 +0700

    Thêm model Feedback

---
+++ Models/Feedback.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotelManagementAPI.Models;
+
+public partial class Feedback
+{
+    public string MaFeedback { get; set; } = null!;
+
+    public string MaPhong { get; set; } = null!;
+
+    public string MaNguoiDung { get; set; } = null!;
+
+    public int SoSao { get; set; }
+
+    public string? BinhLuan { get; set; }
+
+    public DateTime? NgayFeedback { get; set; }
+
+    public virtual NguoiDung MaNguoiDungNavigation { get; set; } = null!;
+
+    public virtual Phong MaPhongNavigation { get; set; } = null!;
+}
