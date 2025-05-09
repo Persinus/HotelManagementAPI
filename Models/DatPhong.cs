3ec0bca1c034561114881fbf60dab2de5e2b9e1c
Persinus <nguyenmanh2004devgame@gmail.com>
2025-05-10 02:41:42 +0700

    Modify Models/DatPhong.cs

--- Models/DatPhong.cs
+++ Models/DatPhong.cs
@@ -7,25 +7,23 @@ public partial class DatPhong
 {
     public string MaDatPhong { get; set; } = null!;
 
-    public string MaKhachHang { get; set; } = null!;
-
-    public string MaNhanVien { get; set; } = null!;
+    public string MaNguoiDung { get; set; } = null!;
 
     public string MaPhong { get; set; } = null!;
 
-    public DateTime NgayDat { get; set; }
+    public DateTime? NgayDat { get; set; }
 
-    public DateTime NgayNhanPhong { get; set; }
+    public DateTime? NgayCheckIn { get; set; }
 
-    public DateTime NgayTraPhong { get; set; }
+    public DateTime? NgayCheckOut { get; set; }
 
-    public int TrangThai { get; set; }
+    public string TinhTrangDatPhong { get; set; } = null!;
 
-    public virtual ICollection<HoaDonThanhToanPhong> HoaDonThanhToanPhongs { get; set; } = new List<HoaDonThanhToanPhong>();
+    public virtual ICollection<DatDichVu> DatDichVus { get; set; } = new List<DatDichVu>();
 
-    public virtual KhachHang MaKhachHangNavigation { get; set; } = null!;
+    public virtual ICollection<HoaDon> HoaDons { get; set; } = new List<HoaDon>();
 
-    public virtual NhanVien MaNhanVienNavigation { get; set; } = null!;
+    public virtual NguoiDung MaNguoiDungNavigation { get; set; } = null!;
 
-    public virtual PhongWithTienNghi MaPhongNavigation { get; set; } = null!;
+    public virtual Phong MaPhongNavigation { get; set; } = null!;
 }
