f2a022e62a2284a178d9658875c848ed121d3056
MisterHuy2309 <huygia2309@gmail.com>
2025-05-12 02:44:46 +0700

    Cập nhật: DTOs/TienNghiDTO.cs

--- DTOs/TienNghiDTO.cs
+++ DTOs/TienNghiDTO.cs
@@ -12,7 +12,6 @@ namespace HotelManagementAPI.DTOs
         [StringLength(100)]
         public string TenTienNghi { get; set; } = null!;
 
-        [StringLength(500)]
         public string? MoTa { get; set; }
     }
 }
