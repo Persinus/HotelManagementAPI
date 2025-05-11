bb70db012cce1dc144ac7988ba4386a098425836
Persinus <nguyenmanh2004devgame@gmail.com>
2025-05-11 14:24:37 +0700

    Update DatPhongDTO to reflect booking structure changes

--- DTOs/DatPhongDTO.cs
+++ DTOs/DatPhongDTO.cs
@@ -1,13 +1,33 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace HotelManagementAPI.DTOs
 {
     public class DatPhongDTO
     {
+        [Required]
+        [StringLength(10)]
         public string MaDatPhong { get; set; } // Tự động tạo
+
+        [Required]
+        [StringLength(10)]
         public string MaNguoiDung { get; set; } // Bắt buộc
+
+        [Required]
+        [StringLength(10)]
         public string MaPhong { get; set; } // Bắt buộc
+
+        [Required]
         public DateTime? NgayDat { get; set; } // Bắt buộc
+
+        [Required]
         public DateTime? NgayCheckIn { get; set; } // Bắt buộc
+
+        [Required]
         public DateTime? NgayCheckOut { get; set; } // Bắt buộc
+
+        [Required]
+        [StringLength(20)]
         public string TinhTrangDatPhong { get; set; } // Bắt buộc
     }
 }
