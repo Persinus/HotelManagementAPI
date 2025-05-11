f88d820e587dae629d0504a1abc8e0803df484b7
MisterHuy2309 <huygia2309@gmail.com>
2025-05-12 02:49:04 +0700

    Cập nhật: obj/Debug/net9.0/HotelManagementAPI.AssemblyInfo.cs

--- obj/Debug/net9.0/HotelManagementAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/HotelManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f04afba94cad8506000cc27f6ca090c0efc094e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e2fcb0077c6006afa4ddb627b4c9a74fca55eac")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
