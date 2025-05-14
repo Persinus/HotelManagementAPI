f5c169c22300ed0c41fbf55f0cb5f8e3a7729908
MisterHuy2309 <huygia2309@gmail.com>
2025-05-15 00:21:18 +0700

    Huy code

---
+++ Controllers/NhanVien/DichVuQuanLyController.cs
@@ -0,0 +1,9 @@
+//Mục đích: Quản lý dịch vụ
+
+//GET /api/dichvu – Danh sách dịch vụ
+
+//POST /api/dichvu – Thêm dịch vụ-- cái này cho quản trị viên
+
+//PUT /api/dichvu/{id} – Cập nhật dịch vụ
+
+//DELETE /api/dichvu/{id} – Xóa dịch vụ-- cái này cho quản trị viên
