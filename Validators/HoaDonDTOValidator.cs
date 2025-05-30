601cde3c171a0ccd62f2c26e67039a983026db1f
Persinus <nguyenmanh2004devgame@gmail.com>
2025-05-30 23:05:54 +0700

    Add HoaDonDTOValidator.cs

---
+++ Validators/HoaDonDTOValidator.cs
@@ -0,0 +1,26 @@
+using FluentValidation;
+using HotelManagementAPI.DTOs;
+
+public class HoaDonDTOValidator : AbstractValidator<HoaDonDTO>
+{
+    public HoaDonDTOValidator()
+    {
+        RuleFor(x => x.MaHoaDon)
+            .NotEmpty().WithMessage("Mã hóa đơn là bắt buộc.")
+            .Length(6).WithMessage("Mã hóa đơn phải đúng 6 ký tự.");
+
+        RuleFor(x => x.MaNguoiDung)
+            .NotEmpty().WithMessage("Mã người dùng là bắt buộc.")
+            .Length(6).WithMessage("Mã người dùng phải đúng 6 ký tự.");
+
+        RuleFor(x => x.MaDatPhong)
+            .NotEmpty().WithMessage("Mã đặt phòng là bắt buộc.")
+            .Length(6).WithMessage("Mã đặt phòng phải đúng 6 ký tự.");
+
+        RuleFor(x => x.TongTien)
+            .GreaterThanOrEqualTo(0).WithMessage("Tổng tiền phải lớn hơn hoặc bằng 0.");
+
+        RuleFor(x => x.TinhTrangHoaDon)
+            .NotEmpty().WithMessage("Tình trạng hóa đơn là bắt buộc.");
+    }
+}
