3eb8ae7a69d8b5306d2794f3dad343334eaf59bb
Persinus <nguyenmanh2004devgame@gmail.com>
2025-05-31 22:06:23 +0700

    Xóa controller cũ: QuanTriNhanVienController.cs

--- Controllers/QuanTriVien/QuanTriNhanVienController.cs
+++
@@ -1,9 +0,0 @@
-//Mục đích: Quản lý nhân viên
-
-//POST /api/admin/nhanvien – Thêm nhân viên
-
-//GET /api/admin/nhanvien – Danh sách nhân viên
-
-//PUT /api/admin/nhanvien/{id} – Cập nhật thông tin nhân viên
-
-//DELETE /api/admin/nhanvien/{id} – Xóa nhân viên   
