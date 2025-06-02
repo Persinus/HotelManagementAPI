2e1d10acd0c0547075869ac91c449bc2e847b4e7
Persinus <nguyenmanh2004devgame@gmail.com>
2025-06-03 05:09:20 +0700

    Add DangNhapDTO: replaces old LoginDTO for better naming

---
+++ DTOs/Chung/DangNhapDTO.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+namespace HotelManagementAPI.DTOs
+{
+    public class LoginDTO
+    {
+       
+        public string TenTaiKhoan { get; set; } // Bắt buộc
+      
+        public string MatKhau { get; set; } // Bắt buộc
+    }
+}
