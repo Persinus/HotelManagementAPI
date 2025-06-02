aad7100feb66b0bd37c587f55ae2191507ab8ba3
Persinus <nguyenmanh2004devgame@gmail.com>
2025-06-02 18:16:00 +0700

    Add new model: NoiQuy

---
+++ Models/NoiQuy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotelManagementAPI.Models;
+
+public partial class NoiQuy
+{
+    public int Id { get; set; }
+
+    public int SoThuTu { get; set; }
+
+    public string TieuDe { get; set; } = null!;
+
+    public string NoiDung { get; set; } = null!;
+
+    public string? HinhAnh { get; set; }
+}
