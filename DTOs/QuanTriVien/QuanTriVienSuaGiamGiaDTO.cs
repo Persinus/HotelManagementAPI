a7abc413c40be2dce3db6e3f7de75cf9575be6d1
Persinus <nguyenmanh2004devgame@gmail.com>
2025-06-06 04:24:21 +0700

    Thêm DTO để chỉnh sửa thông tin giảm giá

---
+++ DTOs/QuanTriVien/QuanTriVienSuaGiamGiaDTO.cs
@@ -0,0 +1,13 @@
+namespace HotelManagementAPI.DTOs.QuanTriVien
+{
+    public class QuanTriVienSuaGiamGiaDTO
+    {
+        public string MaGiamGia { get; set; } = null!;
+        public string TenGiamGia { get; set; } = null!;
+        public string? LoaiGiamGia { get; set; }
+        public decimal GiaTriGiam { get; set; }
+        public DateTime NgayBatDau { get; set; }
+        public DateTime NgayKetThuc { get; set; }
+        public string? MoTa { get; set; }
+    }
+}
