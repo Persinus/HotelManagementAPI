606a151e2b718aad3def7dc4714b9677302102bc
Persinus <nguyenmanh2004devgame@gmail.com>
2025-06-06 17:56:48 +0700

    Chỉnh sửa KhachHangHoaDonDTO.cs

--- DTOs/KhachHang/KhachHangHoaDonDTO.cs
+++ DTOs/KhachHang/KhachHangHoaDonDTO.cs
@@ -2,7 +2,7 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
-namespace HotelManagementAPI.DTOs
+namespace HotelManagementAPI.DTOs.KhachHang
 {
     public class KhachHangHoaDonDTO
     {
