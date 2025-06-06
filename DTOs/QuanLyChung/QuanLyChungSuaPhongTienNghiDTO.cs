470715bf0baa1b6caa1993d8722e59980c2c6533
Persinus <nguyenmanh2004devgame@gmail.com>
2025-06-06 17:55:06 +0700

    Thêm file QuanLyChungSuaPhongTienNghiDTO.cs

---
+++ DTOs/QuanLyChung/QuanLyChungSuaPhongTienNghiDTO.cs
@@ -0,0 +1,11 @@
+// Project: QuanLyChung
+// File: DTOs/QuanLyChung/NhanVienSuaPhongTienNghiDTO.cs
+using System.ComponentModel.DataAnnotations;
+// HotelManagementAPI.DTOs.QuanLyChung
+namespace HotelManagementAPI.DTOs.QuanLyChung{
+public class QuanLyChungSuaPhongTienNghiDTO
+{
+   public string TenTienNghi { get; set; } = null!;
+
+    public string? MoTa { get; set; }
+}}
