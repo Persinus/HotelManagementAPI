bbd777eecf30190d5e3daf7be6a296f6e9d45d1e
Persinus <nguyenmanh2004devgame@gmail.com>
2025-06-06 17:56:50 +0700

    Chỉnh sửa QuanTriVienApDungGiamGiaDTO.cs

--- DTOs/QuanTriVien/QuanTriVienApDungGiamGiaDTO.cs
+++ DTOs/QuanTriVien/QuanTriVienApDungGiamGiaDTO.cs
@@ -5,6 +5,11 @@ namespace HotelManagementAPI.DTOs.QuanTriVien
     public class QuanTriVienApDungGiamGiaDTO
     {
         public string MaGiamGia { get; set; } = null!;
+        public string TenGiamGia { get; set; } = null!;
+        public decimal GiaTriGiam { get; set; }
+        public DateTime NgayBatDau { get; set; }
+        public DateTime NgayKetThuc { get; set; }
+        public string? MoTa { get; set; }
         public List<string> DanhSachMaPhong { get; set; } = new();
     }
 }
