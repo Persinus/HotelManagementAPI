96dc404c26f9c9c963e418a875ea9affa31a10a9
Persinus <nguyenmanh2004devgame@gmail.com>
2025-06-06 17:56:09 +0700

    Thêm file KhachHangFeedBackDTO.cs

---
+++ DTOs/KhachHang/KhachHangFeedBackDTO.cs
@@ -0,0 +1,10 @@
+namespace HotelManagementAPI.DTOs.KhachHang
+{
+    public class KhachHangFeedBackDTO
+    {
+        public string MaPhong { get; set; } = null!;
+        public int SoSao { get; set; }
+        public string? BinhLuan { get; set; }
+     
+    }
+}
