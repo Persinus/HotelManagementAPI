896a7935ee4d97c6444e74a6eec4d5d0f914111d
Persinus <nguyenmanh2004devgame@gmail.com>
2025-06-06 17:56:48 +0700

    Chỉnh sửa KhachHangLichSuGiaoDichDTO.cs

--- DTOs/KhachHang/KhachHangLichSuGiaoDichDTO.cs
+++ DTOs/KhachHang/KhachHangLichSuGiaoDichDTO.cs
@@ -1,7 +1,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 
-namespace HotelManagementAPI.DTOs
+namespace HotelManagementAPI.DTOs.KhachHang
 {
     public class KhachHangLichSuGiaoDichDTO
     {
