6e37947119057ad91871b3989f1498d6af06c6e7
Persinus <nguyenmanh2004devgame@gmail.com>
2025-06-10 07:45:12 +0700

    Fix KhachHangTaoHoaDonRequestDTO logic issues

--- DTOs/KhachHang/KhachHangTaoHoaDonRequestDTO.cs
+++ DTOs/KhachHang/KhachHangTaoHoaDonRequestDTO.cs
@@ -12,7 +12,6 @@ namespace HotelManagementAPI.DTOs.KhachHang
 public class TaoHoaDonRequestDTO
 {
     public string MaDatPhong { get; set; } = null!;
-    public decimal GiaTriGiam { get; set; } // Thêm dòng này
-    // ... các trường khác nếu có ...
+  
 } 
 }
